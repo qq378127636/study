cb8d2ce71d8ac6a5060e730eb3b59a6487621c7b
qq378127636 <378127636@qq.com>
2017-08-27 21:57:03 +0800

    test

---
+++ j2ee/src/utils/Hello.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ * (用一句话描述该文件做什么)
+ *
+ * DaHuanDan
+ * 2017/8/27
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
