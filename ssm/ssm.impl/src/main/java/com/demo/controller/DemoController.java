a073db6482214e005ce299061a8c8272b26796d1
qq378127636 <378127636@qq.com>
2017-11-06 00:24:44 +0800

    框架初始化

--- ssm/ssm.impl/src/main/java/com/demo/controller/DemoController.java
+++ ssm/ssm.impl/src/main/java/com/demo/controller/DemoController.java
@@ -1,7 +1,24 @@
 package com.demo.controller;
 
+import com.demo.service.IDemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by hxb on 2017/11/4
  */
+@RequestMapping("/demo")
+@Controller
 public class DemoController {
+
+    @Autowired
+    private IDemoService iDemoService;
+
+    @RequestMapping("/findName")
+    @ResponseBody
+    public String findName() throws Exception {
+        return iDemoService.findName(1);
+    }
 }
--- ssm/ssm.impl/src/main/java/com/demo/service/impl/IDemoServiceImpl.java
+++ ssm/ssm.impl/src/main/java/com/demo/service/impl/IDemoServiceImpl.java
@@ -1,14 +1,24 @@
 package com.demo.service.impl;
 
+import com.demo.dao.IDemoDao;
 import com.demo.service.IDemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
 
 /**
  * Created by hxb on 2017/11/4
  */
+@Service
 public class IDemoServiceImpl implements IDemoService{
+
+    @Autowired
+    private IDemoDao iDemoDao;
 
     @Override
     public String findName(Integer id) {
-        return null;
+
+        return iDemoDao.findName(id);
     }
 }
